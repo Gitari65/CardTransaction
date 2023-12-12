e6f6bd32ac938ba6f04a3c1cd049bdd4f2c1afdb
Gitari65 <agitari65@gmail.com>
2023-12-12 06:06:06 +0300

    models

---
+++ server/models/user.js
@@ -0,0 +1,24 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+const bcrypt=require('bcrypt');
+
+const userSchema= new Schema({
+    username:{type:String,unique:true, required:true},
+
+    password:{type:String, required:true}
+})
+
+userSchema.pre('save',async function(next){
+    try {
+        const salt =await bcrypt.genSalt(10);
+        const hashedPassword= await bcrypt.hash(this.password,salt);
+        this.password=hashedPassword;
+        next();
+    } catch (error) {
+        next(error);
+    }
+    userSchema.methods.comparePassword=async function (password) {
+        return bcrypt.compare(password,this.password);
+    }
+})
+module.exports=mongoose.model('User',userSchema)
