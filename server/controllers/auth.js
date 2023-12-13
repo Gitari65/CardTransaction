a2609abe900c04f6734429221d673e28cb860b6f
Gitari65 <agitari65@gmail.com>
2023-12-13 09:18:01 +0300

    message

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -12,9 +12,29 @@ exports.signup= async (req,res)=>{
 
     } catch (error) {
         res.status(500).json({error:'internal server error'})
+
         
     }
 };
 
 
-exports.login
+exports.login=async (req,res)=>{
+    try {
+        const {username,password}=req.body;
+        const user=User.findOne({username});
+        if(!user){
+            res.status(401).json({error:"invalid username"})
+        }
+        const isPasswordValid=await user.comparePassword(password);
+        if(!isPasswordValid){
+            return res.status(401).json({error:" invalid password"})
+        }
+        const token=jwt.sign({userId:user._id},'secret-key',{expiresIn:'1hr'} );
+        res.json({user,token});
+
+    } catch (error) {
+        return res.status(500).json({error:" internal server error"})
+        
+    }
+
+}
